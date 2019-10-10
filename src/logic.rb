b68ae4fc24212e1013bf2fdb24388c2573fab4b5
PraneethaML <praneetha1993@gmail.com>
2019-10-10 23:09:41 +0530

    Add input file and test logic

--- src/logic.rb
+++ src/logic.rb
@@ -1,13 +1,39 @@
 class Logic
-	def read_input
-		
+	
+	def initialize
+		if ARGV.length == 1
+			filename = ARGV.first
+			is_file_exists = File.file?(filename)
+			puts filename
+			puts is_file_exists
+			if is_file_exists
+				read_input(filename)
+			else
+				puts "File #{filename} does not exist"
+			end
+		else
+			puts "Input filename must be given as an argument"
+		end
 	end
 
-	def validate_input
+	def read_input(filename)
 		
+		# print no file found for invalid or no file available
+		# read the file contents line by line if found
+
+		puts "yay"
+
 	end
 
-	def process_input
+	# def validate_input
 		
-	end
+	# end
+
+	# def process_input
+		
+	# end
+end
+
+begin 
+Logic.new
 end
