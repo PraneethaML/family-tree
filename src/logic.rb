64d41fc1aa2552354d7391ed39f8391c9cdde080
PraneethaML <praneetha1993@gmail.com>
2019-10-13 01:00:11 +0530

    Update logic.rb

--- src/logic.rb
+++ src/logic.rb
@@ -1,13 +1,15 @@
+load 'family_tree.rb'
+load 'create_shan_family_tree.rb'
+
 class Logic
 	
-	def initialize
+	def initialize(tree)
 		if ARGV.length == 1
 			filename = ARGV.first
 			is_file_exists = File.file?(filename)
-			puts filename
-			puts is_file_exists
 			if is_file_exists
-				read_input(filename)
+				@tree = tree
+				process_input(filename)
 			else
 				puts "File #{filename} does not exist"
 			end
@@ -16,32 +18,42 @@ class Logic
 		end
 	end
 
-	def read_input(filename)
-	 	File.open("input.txt").each do |input|
+	def process_input(filename)
+	 	File.open(filename).each do |input|
 	 		command, *params = input.split /\s/
-    		is_valid_input = validate_input(command,params)
+    		is_valid_input, message = validate_input(command,params)
+    		if is_valid_input
+    			case command
+    			 when 'ADD_CHILD'
+    			 	@family_tree.add_child(params)
+    			 when 'GET_RELATIONSHIP'
+    			 	@family_tree.get_relationship(params)
+    			 end			
+    		else 
+    			puts message
+    		end
 	 	end
 	end
 
 	def validate_input(command,params)
-		@family_tree = FamilyTree.new
+		@family_tree = FamilyTree.new(@tree)
 		case command
-		when ADD_CHILD 
+		when 'ADD_CHILD' 
 			if params.length != 3
 				return false, "3 parameters required"
 			else
 				mother_name = params[0]
 				child_name = params[1]
 				child_gender = params[2]
-				mother_node = @family_tree.validate_member(mother_name)
+				mother_node = @family_tree.get_member(mother_name)
 				
-				return false if mother_node.nil?
-				return false if mother_node.content[:gender] != 'female'
+				return false, "CHILD_ADDITION_FAILED" if mother_node.nil?
+				return false, "CHILD_ADDITION_FAILED" if mother_node.content[:gender] != 'female'
 
-				return true		
+				return true	
 			end
-		when GET_RELATIONSHIP 
-			puts "pass"
+		when 'GET_RELATIONSHIP' 
+			return true
 		else 
 			return false, "Invalid input command"
 		end
@@ -49,5 +61,7 @@ class Logic
 end
 
 begin 
-Logic.new
+c = CreateShanFamilyTree.new
+tree = c.create_basic_tree
+Logic.new(tree)
 end
