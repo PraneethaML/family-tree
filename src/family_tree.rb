2fb73241c92502445419269755234f2efe18115f
PraneethaML <praneetha1993@gmail.com>
2019-10-13 00:04:40 +0530

    Update family_tree.rb

--- src/family_tree.rb
+++ src/family_tree.rb
@@ -7,4 +7,38 @@ class FamilyTree
 	def get_relationship
 		
 	end
+
+	def get_gender
+		
+	end
+
+	def get_siblings
+		
+	end
+
+	def get_children
+		
+	end
+
+	def get_paternal_uncle
+		
+	end
+
+	def get_maternal_uncle
+		
+	end
+
+	def get_paternal_aunt
+		
+	end
+
+	def get_maternal_aunt
+		
+	end
+
+	def get_member(member_name)
+		anga.detect { |node|
+			node.name.downcase == member_name.downcase 
+		}
+	end
 end
