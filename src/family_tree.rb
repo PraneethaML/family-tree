5838b929e2ee5115c24bd76422a7b05386b42796
PraneethaML <praneetha1993@gmail.com>
2019-10-14 00:47:46 +0530

    Add code

--- src/family_tree.rb
+++ src/family_tree.rb
@@ -16,7 +16,6 @@ class FamilyTree
 	end
 	
 	def add_child(params)
-		puts "hey you are in add child function of family tree and params are #{params}"
 		add_child_hash = {}
 		add_child_hash['mothers_name'] = params[0].downcase
 		add_child_hash['child_name'] = params[1].downcase
@@ -27,40 +26,196 @@ class FamilyTree
 	end
 
 	def get_relationship(params)
-		puts "hey you are in get get_relationship of family tree and params are #{params}"
+		person = params[0]
+		relation = params[1]
+		case relation
+		when 'Siblings'
+			get_siblings(person)
+		when 'Daughter'
+			get_daughter(person)
+		when 'Son'
+			get_son(person)
+		when 'Brother-In-Law'
+			get_brother_in_law(person)
+		when 'Sister-In-Law'
+			get_sister_in_law(person)
+		when 'Maternal-Aunt'
+			get_maternal_aunt(person)
+		when 'Paternal-Aunt'
+			get_paternal_aunt(person)
+		when 'Maternal-Uncle'
+			get_maternal_uncle(person)
+		when 'Paternal-Uncle'
+			get_paternal_uncle(person)
+		else
+			puts "Sorry! we will get back to you later"
+		end
 	end
 
 
 	def get_siblings(member_name)
-		all_siblings = get_member(member_name).siblings
-
-
-		
+		siblings = []
+		all_siblings = get_member(member_name).siblings	
+		all_siblings.each { |sib|
+			siblings << sib.name if sib.content[:relation] == 'child'		
+		}
+		if siblings.empty?
+			puts "NONE"
+		else
+			puts siblings.join(' ')
+		end
 	end
 
-	def get_children(member_name)
+	def get_daughter(member_name)
+		daughters = []
 		all_children = get_member(member_name).children
+		all_children.each { |node| 
+			daughters << node.name if node.content[:relation] == 'child' && node.content[:gender] == 'female'
+		  }
+
+		if daughters.empty?
+			puts "NONE"
+		else
+			puts daughters.join(' ')
+		end
+	end
+
+	def get_son(member_name)
+		sons = []
+		all_children = get_member(member_name).children
+		all_children.each { |node| 
+			sons << node.name if node.content[:relation] == 'child' && node.content[:gender] == 'male'
+		  }
+
+		if sons.empty?
+			puts "NONE"
+		else
+			puts sons.join(' ')
+		end
 	end
 
 	def get_paternal_uncle(member_name)
-		
+		person = get_member(member_name)
+		paternal_uncles = []
+		if person.parent.content[:relation] == 'spouse'
+			father = sofi.parent.parent
+			uncles = father.siblings
+			uncles.each do |uncle|
+				paternal_uncles << uncle.name if uncle.content[:gender] == 'male' && uncle.content[:relation] == 'child'
+			end
+		end
+		if paternal_uncles.empty?
+			puts "NONE"
+		else
+			puts paternal_uncles.join(' ')
+		end
+
 	end
 
 	def get_maternal_uncle(member_name)
+		person = get_member(member_name)
+		maternal_uncles = []
 		
+		mother = person.parent
+		uncles = mother.siblings
+		uncles.each do |uncle|
+			maternal_uncles << uncle.name if uncle.content[:gender] == 'male' && uncle.content[:relation] == 'child'
+		end
+		
+		if maternal_uncles.empty?
+			puts "NONE"
+		else
+			puts maternal_uncles.join(' ')
+		end
 	end
 
 	def get_paternal_aunt(member_name)
-		
+		person = get_member(member_name)
+		paternal_aunts = []
+		if person.parent.content[:relation] == 'spouse'
+			father = person.parent.parent
+			aunts = father.siblings
+			aunts.each do |aunt|
+				paternal_aunts << aunt.name if aunt.content[:gender] == 'female' && aunt.content[:relation] == 'child'
+			end
+		end
+		if paternal_uncles.empty?
+			puts "NONE"
+		else
+			puts paternal_uncles.join(' ')
+		end
 	end
 
 	def get_maternal_aunt(member_name)
+		person = get_member(member_name)
+		maternal_aunts = []
+		mother = person.parent
+		aunts = mother.siblings
+		aunts.each { |aunt|
+			maternal_aunts << aunt.name if aunt.content[:gender] == 'female' && aunt.content[:relation] == 'child'
+		}
 		
+		if maternal_aunts.empty?
+			puts "NONE"
+		else
+			puts maternal_aunts.join(' ')
+		end
+	end
+
+	# def get_brother_in_law(member_name)
+	# 	brother_in_laws = []
+	# 	person = get_member(member_name)
+	# 	spouse = person.parent if person.parent.content[:relation] == 'spouse'
+	# 	spouse_sibllings = spouse.siblings	
+		
+	# 	spouse_sibllings.each { |ss|
+	# 	  	if ss.content[:gender] == 'male' && ss.content[:relation] == 'child'
+	# 	  		brother_in_laws << ss.name if ss.content[:gender] == 'male' && ss.content[:relation] == 'child'
+	# 	  	elsif ss.content[:gender] == 'female' && ss.content[:relation] == 'child'		
+	# 	  		spouse_sister = get_member(ss.name)
+	# 	  		spouse_sister_children_nodes = spouse_sister.children
+		  		
+	# 	  		spouse_sister_children_nodes.each do |node|
+	# 	  			brother_in_laws << if node.content[:relation] == 'spouse' && node.content[:gender] == 'male'
+	# 	  		end
+	# 	  	end
+	# 	}
+
+	# 	if brother_in_laws.empty?
+	# 		puts "NONE"
+	# 	else
+	# 		puts brother_in_laws.join(' ')
+	# 	end
+	# end
+
+	def get_sister_in_law(member_name)
+		sister_in_laws = []
+		person = get_member(member_name)
+		spouse = person.parent if person.parent.content[:relation] == 'spouse'
+		spouse_sibllings = spouse.siblings	
+		spouse_sibllings.each do |ss|
+		  	if ss.content[:gender] == 'female' && ss.content[:relation] == 'child'
+		  		sister_in_laws << ss.name if ss.content[:gender] == 'female' && ss.content[:relation] == 'child'
+		  	elsif ss.content[:gender] == 'male' && ss.content[:relation] == 'child'		
+		  		spouse_brother = get_member(ss.name)
+		  		spouse_brother_children_nodes = spouse_brother.children
+		  		
+		  		spouse_brother_children_nodes.each do |node|
+		  			sister_in_laws << if node.content[:relation] == 'spouse' && node.content[:gender] == 'female'
+		  		end
+		  	end
+		end	
+
+		if sister_in_laws.empty?
+			puts "NONE"
+		else
+			puts sister_in_laws.join(' ')
+		end
 	end
 
 	def get_member(member_name)
-		@root.detect { |node|
+		@root.detect do |node|
 			node.name.downcase == member_name.downcase 
-		}
+		end
 	end
 end
