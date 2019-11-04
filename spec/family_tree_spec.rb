b1a0e143e47b669937e900949fa6d2fbaa1e2a04
PraneethaML <praneetha1993@gmail.com>
2019-11-04 20:40:03 +0530

    remove redundancy using let variables

--- spec/family_tree_spec.rb
+++ spec/family_tree_spec.rb
@@ -4,18 +4,21 @@ require '../src/create_initial_family.rb'
 
 RSpec.describe FamilyTree do
 
+  let(:family) { CreateInitialFamily.new }
+  let(:tree) { FamilyTree.new(family) }
+    
   it 'creates a FamilyTree class' do
-    family = CreateInitialFamily.new
-    tree = FamilyTree.new(family)
+    # family = CreateInitialFamily.new
+    # tree = FamilyTree.new(family)
     expect(tree).to be_kind_of(FamilyTree)
   end
 
   it 'creates relation' do
-    family = CreateInitialFamily.new
+    # family = CreateInitialFamily.new
     family.create_root('root_name')
-    tree = FamilyTree.new(family)
+    # tree = FamilyTree.new(family)
     expect(tree).to respond_to(:create_relation)
-    
+
     params = ['Anga', 'Vich', 'male']
     value = tree.create_relation(params)
     expect(value).to be false
@@ -26,20 +29,20 @@ RSpec.describe FamilyTree do
   end
 
   it 'gets relationship' do
-    family = CreateInitialFamily.new
-    tree = FamilyTree.new(family)
-    expect(tree).to respond_to(:get_relationship)	
+    # family = CreateInitialFamily.new
+    # tree = FamilyTree.new(family)
+    expect(tree).to respond_to(:get_relationship)
   end
 
- 
   it 'gets members' do
-    family = CreateInitialFamily.new
+    # family = CreateInitialFamily.new
     family.create_root('root_name')
-    tree = FamilyTree.new(family)
+    # tree = FamilyTree.new(family)
     expect(tree).to respond_to(:get_member)
     # return member object if exists
     expect(tree.get_member('root_name')).to be_kind_of(Tree::TreeNode)
     # return nil incase member doesn't exist
     expect(tree.get_member('rut_name')).to be_nil
   end
+
 end
