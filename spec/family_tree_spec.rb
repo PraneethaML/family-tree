6503892265a993ea64f20402cbff2c7e0b966ba2
PraneethaML <praneetha1993@gmail.com>
2019-11-05 23:25:04 +0530

    Add tests for private methods

--- spec/family_tree_spec.rb
+++ spec/family_tree_spec.rb
@@ -1,4 +1,5 @@
 require 'rspec'
+require 'byebug'
 require '../src/family_tree.rb'
 require '../src/create_initial_family.rb'
 
@@ -6,42 +7,34 @@ RSpec.describe FamilyTree do
 
   let(:family) { CreateInitialFamily.new }
   let(:tree) { FamilyTree.new(family) }
-    
+
   before(:each) do
     FamilyTree.send(:public, *FamilyTree.private_instance_methods)
   end
 
   it 'creates a FamilyTree class' do
-    # family = CreateInitialFamily.new
-    # tree = FamilyTree.new(family)
     expect(tree).to be_kind_of(FamilyTree)
   end
 
   it 'creates relation' do
-    # family = CreateInitialFamily.new
     family.create_root('root_name')
-    # tree = FamilyTree.new(family)
     expect(tree).to respond_to(:create_relation)
 
-    params = ['Anga', 'Vich', 'male']
+    params = %w[Anga Vich male]
     value = tree.create_relation(params)
     expect(value).to be false
-    
-    params = ['root_name', 'Vich', 'male']
+
+    params = %w[root_name Vich male]
     value = tree.create_relation(params)
     expect(value).to be true
   end
 
   it 'gets relationship' do
-    # family = CreateInitialFamily.new
-    # tree = FamilyTree.new(family)
     expect(tree).to respond_to(:get_relationship)
   end
 
   it 'gets members' do
-    # family = CreateInitialFamily.new
     family.create_root('root_name')
-    # tree = FamilyTree.new(family)
     expect(tree).to respond_to(:get_member)
     # return member object if exists
     expect(tree.get_member('root_name')).to be_kind_of(Tree::TreeNode)
@@ -49,9 +42,61 @@ RSpec.describe FamilyTree do
     expect(tree.get_member('rut_name')).to be_nil
   end
 
-   it 'creates member' do
-    # family = CreateInitialFamily.new
-    # tree = FamilyTree.new(family)
+  it 'creates member' do
     expect(tree).to respond_to(:create_member)
   end
+
+  it 'gets siblings' do
+    expect(tree).to respond_to(:get_siblings)
+  end
+
+  it 'print output' do
+    expect(tree).to respond_to(:print_output)
+  end
+
+  it 'gets children' do
+    expect(tree).to respond_to(:get_children)
+  end
+
+  it 'gets uncles' do
+    expect(tree).to respond_to(:get_uncles)
+  end
+
+  it 'gets paternal uncles' do
+    expect(tree).to respond_to(:get_paternal_uncle)
+  end
+
+  it 'gets maternal uncles' do
+    expect(tree).to respond_to(:get_maternal_uncle)
+  end
+
+  it 'gets aunts' do
+    expect(tree).to respond_to(:get_aunts)
+  end
+
+  it 'gets paternal aunt' do
+    expect(tree).to respond_to(:get_paternal_aunt)
+  end
+
+  it 'gets maternal aunt' do
+    expect(tree).to respond_to(:get_maternal_aunt)
+  end
+
+  it 'gets brothers sisters' do
+    expect(tree).to respond_to(:brothers_sisters)
+  end
+
+  it 'gets spouse' do
+    expect(tree).to respond_to(:get_spouse)
+  end
+
+  it 'gets relatives' do
+    expect(tree).to respond_to(:relation_wrt_spouse)
+    expect(tree).to respond_to(:relation_wrt_child)
+  end
+
+  it 'gets relation' do
+    expect(tree).to respond_to(:get_relation)
+  end
+
 end
