8e1ef4c79273d3475fbd71dce0efd7351c3736d4
PraneethaML <praneetha1993@gmail.com>
2019-11-09 18:16:09 +0530

    Add tests for private methods

--- spec/create_initial_family_spec.rb
+++ spec/create_initial_family_spec.rb
@@ -2,25 +2,40 @@ require 'rspec'
 require '../src/create_initial_family.rb'
 
 RSpec.describe CreateInitialFamily do
+
+  let(:family) { CreateInitialFamily.new }
+
+  before(:each) do
+    CreateInitialFamily.send(:public, *CreateInitialFamily.private_instance_methods)
+  end
+
   it 'creates a CreateInitialFamily class' do
-    family = CreateInitialFamily.new
     expect(family).to be_kind_of(CreateInitialFamily)
   end
 
   it 'creates a root' do
-    family = CreateInitialFamily.new
     expect(family).to respond_to(:create_root)
   end
 
   it 'creates basic family' do
-    family = CreateInitialFamily.new
     expect(family).to respond_to(:create_basic_family)
   end
 
   it 'gets root' do
-    family = CreateInitialFamily.new
     expect(family).to respond_to(:get_root)
     family.create_root('root')
     expect(family.get_root).to be_kind_of(Tree::TreeNode)
+  end
+
+  it 'add_to_family' do
+    expect(family).to respond_to(:add_to_family)
+  end
+
+  it 'gets member' do
+    expect(family).to respond_to(:get_member)
+  end 
+
+  it 'prints tree' do 
+    expect(family).to respond_to(:print_tree)
   end
 end
--- spec/family_tree_spec.rb
+++ spec/family_tree_spec.rb
@@ -42,12 +42,38 @@ RSpec.describe FamilyTree do
     expect(tree.get_member('rut_name')).to be_nil
   end
 
-  it 'creates member' do
+  it 'responds to creates member' do
     expect(tree).to respond_to(:create_member)
+  end
+
+  it 'creates member if mother is present' do 
+    family.create_root('root_name')
+    params = %w[root_name Vich male]
+    create_relation_hash = {}
+    create_relation_hash['mothers_name'] = params[0].downcase
+    create_relation_hash['child_name'] = params[1].downcase
+    create_relation_hash['gender'] = params[2].downcase
+    create_relation_hash['relation'] = 'child'
+    mem_created = tree.create_member(create_relation_hash)
+    expect(mem_created).to be true
+  end
+
+  it 'doesnt create member if mother is not present' do
+    family.create_root('root_name')
+    params = %w[root1_namssse Vich male]
+    create_relation_hash = {}
+    create_relation_hash['mothers_name'] = params[0].downcase
+    create_relation_hash['child_name'] = params[1].downcase
+    create_relation_hash['gender'] = params[2].downcase
+    create_relation_hash['relation'] = 'child'
+    mem_created = tree.create_member(create_relation_hash)
+    expect(mem_created).to be false
   end
 
   it 'gets siblings' do
     expect(tree).to respond_to(:get_siblings)
+    family.create_root('root_name')
+    expect(tree.get_siblings('root_name')).to be_an_instance_of(Array)
   end
 
   it 'print output' do
@@ -56,30 +82,37 @@ RSpec.describe FamilyTree do
 
   it 'gets children' do
     expect(tree).to respond_to(:get_children)
+    expect(tree.get_children('root_name','daughter')).to be_an_instance_of(Array)
   end
 
   it 'gets uncles' do
     expect(tree).to respond_to(:get_uncles)
+    expect(tree.get_uncles('root_name')).to be_an_instance_of(Array)
   end
 
   it 'gets paternal uncles' do
     expect(tree).to respond_to(:get_paternal_uncle)
+    expect(tree.get_paternal_uncle('root_name')).to be_an_instance_of(Array)
   end
 
   it 'gets maternal uncles' do
     expect(tree).to respond_to(:get_maternal_uncle)
+    expect(tree.get_maternal_uncle('root_name')).to be_an_instance_of(Array)
   end
 
   it 'gets aunts' do
     expect(tree).to respond_to(:get_aunts)
+    expect(tree.get_aunts('root_name')).to be_an_instance_of(Array)
   end
 
   it 'gets paternal aunt' do
     expect(tree).to respond_to(:get_paternal_aunt)
+    expect(tree.get_paternal_aunt('root_name')).to be_an_instance_of(Array)
   end
 
   it 'gets maternal aunt' do
     expect(tree).to respond_to(:get_maternal_aunt)
+    expect(tree.get_maternal_uncle('root_name')).to be_an_instance_of(Array)
   end
 
   it 'gets brothers sisters' do
@@ -98,5 +131,4 @@ RSpec.describe FamilyTree do
   it 'gets relation' do
     expect(tree).to respond_to(:get_relation)
   end
-
 end
